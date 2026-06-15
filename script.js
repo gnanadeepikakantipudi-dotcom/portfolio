2e499d33cfdbf456892c9f4aff385f01b6a5d4f2
Hema J <hemajagadeesh468@gmail.com>
2026-06-15 17:59:33 +0530

    my portfolio

---
+++ script.js
@@ -0,0 +1,11 @@
+// Welcome Message
+
+console.log("Portfolio Website Loaded Successfully!");
+
+// Simple button action
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+    alert("Replace this button with your Resume PDF link.");
+});
